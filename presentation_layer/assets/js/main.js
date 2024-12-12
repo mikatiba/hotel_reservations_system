e0b6fd3a6fc2a393a0a512dc9a668e46a7416793
Mikael Tiba <mikaeltiba@MacBookAir.arecibo.inter.edu>
2024-12-12 12:20:02 -0400

    Update presentation layer

--- presentation_layer/assets/js/main.js
+++ presentation_layer/assets/js/main.js
@@ -2,3 +2,36 @@
 $(document).ready(function() {
     // Código común para todas las páginas
     });
+
+    function saveToken(token) {
+        localStorage.setItem("token", token);
+    }
+    
+    function getToken() {
+        return localStorage.getItem("token");
+    }
+    
+    function isLoggedIn() {
+        return !!getToken();
+    }
+    
+    function logout() {
+        localStorage.removeItem("token");
+        window.location.href = "login.html";
+    }
+    
+    $(document).ready(function () {
+        if (isLoggedIn()) {
+            $(".navbar-nav").html(`
+                <li class="nav-item">
+                    <a class="nav-link" href="cliente/perfil.html">Mi Perfil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="logout" href="#">Cerrar Sesión</a>
+                </li>
+            `);
+    
+            $("#logout").on("click", logout);
+        }
+    });
+    
