9f0178a6af69b77f1a7175ad3c34181b9dd65938
Johhny Mercado <jmercado7908@arecibointer.edu>
2024-12-12 12:51:49 -0400

    Actualizando imagenes en la base de datos

--- data_layer/sql_scripts/populate_database.sql
+++ data_layer/sql_scripts/populate_database.sql
@@ -8,11 +8,11 @@ VALUES
 -- Insertar datos en la tabla `habitaciones`
 INSERT INTO habitaciones (numero_habitacion, tipo, descripcion, precio, estado, imagen)
 VALUES
-    ('101', 'Sencilla', 'Habitación sencilla con cama individual', 50.00, 'Disponible', '/imagenes_habitaciones/101.jpg'),
-    ('102', 'Doble', 'Habitación doble con dos camas individuales', 75.00, 'Disponible', '/imagenes_habitaciones/102.jpg'),
-    ('201', 'Matrimonial', 'Habitación matrimonial con cama king size', 100.00, 'Disponible', '/imagenes_habitaciones/201.jpg'),
-    ('202', 'Suite', 'Suite de lujo con vista al mar', 150.00, 'Disponible', '/imagenes_habitaciones/202.jpg'),
-    ('301', 'Presidencial', 'Habitación presidencial con todas las comodidades', 250.00, 'Disponible', '/imagenes_habitaciones/301.jpg');
+    ('101', 'Sencilla', 'Habitación sencilla con cama individual', 50.00, 'Disponible', '/images/101.jpg'),
+    ('102', 'Doble', 'Habitación doble con dos camas individuales', 75.00, 'Disponible', '/images/102.jpg'),
+    ('201', 'Matrimonial', 'Habitación matrimonial con cama king size', 100.00, 'Disponible', '/images/201.jpg'),
+    ('202', 'Suite', 'Suite de lujo con vista al mar', 150.00, 'Disponible', '/images/202.jpg'),
+    ('301', 'Presidencial', 'Habitación presidencial con todas las comodidades', 250.00, 'Disponible', '/images/301.jpg');
 
 -- Insertar datos en la tabla `reservas`
 INSERT INTO reservas (cliente_id, habitacion_id, fecha_inicio, fecha_fin, estado)
